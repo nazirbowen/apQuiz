fa6299654e12d1df866e5bcdf148c8a0a33e12c8
nazirbowen <108888651+nazirbowen@users.noreply.github.com>
2023-09-15 14:35:58 +0000

    im finished my quiz

---
+++ apQuizes/quiz1.py
@@ -0,0 +1,60 @@
+# 1. In your on words, describe what a variable is.
+variables are a type of placeholder for data
+
+# 2. Name three (3) operators we've learned about in class and describe
+#  # a situation where you would use each one.  
+arithmatic operator  - I would use this to do a math problem
+comparison operator - I would use this to compare those values
+assignment operator - I would use this to assign a variable to those values
+
+# 3. Describe the difference between camal case and pascal case.
+Camel Case is the second word of a variable name, and every word after starts with a capital 
+Pascal case every word including the first word, starts with a capital letter.
+
+# 4. Which operator is being used in the example below
+numberOfApples = 10
+numberOfOranges = 12
+numberOfApples < numberOfOranges
+These are Comparison and Logical Operators
+
+# 5. You have been given a $100.00 dollar gift card from a family member. Another 
+# family member adds 50.00 dollars to your gift card and a friend gives you $35.50. 
+# Your task is to create variables for each gift amount and a final variable that
+# represents the total amount of money on your gift card. 
+family_memberA = 100
+family_memberB = 50
+friend = 35.50
+total = family_memberA + family_memberB + friend
+print (total)
+
+
+# 6. Create a variable that uses the input function and then print that variable. Your variable 
+# should ask a user to enter their name.  
+bank_username = input("bank_username:")
+print("bank_username is: " + bank_username)
+
+# 7. You have been asked to create a program for a community center that wants to plan a cookout. 
+# The community center wants you to build a form program and capture several key data points. 
+# They want to know a visitor's full name, their age, if they have an allergy, and what their 
+# favorite food is. You need to create the appropriate varialbes with the appropriate data type
+# and use the input() function and print function to take in and print out the user's responses. 
+
+
+# 8. You have been given a job to create a program for a sandwich shop. The sandwich shop
+# wants to be able to know if a user has a gluten allegery before they make the sandwich order.
+# If the user DOES have a glutten allergy, they need to eat a special type of bread, if the user DOES NOT
+# have an allergy they can eat the regular bread. What data type would best be used in this 
+# scenario and how would you write a variable to represent this value?
+The abstraction data type could be used to break down if the user does or doesn't have an allergy
+GluttenAllergy = SpecialBread
+NonGluttenAllergy = RegularBread
+
+
+# 9. In your own words, describe what a program is?
+A Computer Program is a set of instructions that a computer can do
+in order to perform a task or goal
+
+
+# 10. Describe the difference between a float and an integer. 
+The difference between a integer and a float is an integer is a whole number, non-decimal number , 
+whilst a float is a decimal number. 
